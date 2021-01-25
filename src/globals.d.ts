b009b3f5dafb3eaa05995307244eb97e8c073849
Adam Lesinski <adamlesinski@gmail.com>
2021-01-25 14:38:35 -0800

    Typescriptified index.js

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,18 +1,4 @@
-// Declaration for missing experimental DOM API.
-// [https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver].
-declare class ResizeObserver {
-    constructor(callback: (entries: ResizeObserverEntry[], observer?: ResizeObserver) => void);
-
-    observe(element: Element | SVGElement, options?: ResizeObserverOptions): void;
-    disconnect(): void;
-}
-
-declare interface ResizeObserverEntry {}
-
-declare interface ResizeObserverOptions {
-    box: 'content-box' | 'border-box' | 'device-pixel-content-box';
-}
-
+// Missing property in DOM types.
 declare interface MouseEvent {
     readonly deltaY: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 window.onload = function () {
     var bank = 34;
 
-    const goldSpan = document.getElementById('current-gold');
-    goldSpan.innerText = `${bank}`;
+    const goldSpan = document.getElementById('current-gold')!;
+    goldSpan.textContent = `${bank}`;
 
-    const mapView = document.querySelector('map-view');
+    const mapView = document.querySelector('map-view')!;
 
     mapView.addEventListener('map:selected', event => {
         if (event.target instanceof MapTerritory) {
             const terit = event.target;
-            const tmpl = document.getElementById('territory-control-panel-tmpl');
-            const node = tmpl.content.cloneNode(true);
-            node.querySelector('#territory-name').innerText = terit.name;
+            const tmpl = document.getElementById('territory-control-panel-tmpl')! as HTMLTemplateElement;
+            const node = tmpl.content.cloneNode(true) as HTMLElement;
+            node.querySelector('#territory-name')!.textContent = terit.name;
             if (terit.buildings.length === 0) {
                 if (bank - 6 >= 0) {
-                    const buildOutpostBtn = node.querySelector('#build-outpost-btn');
+                    const buildOutpostBtn = node.querySelector('#build-outpost-btn')!;
                     buildOutpostBtn.removeAttribute('disabled');
                     buildOutpostBtn.addEventListener('click', () => {
                         if (bank - 6 < 0) {
@@ -22,12 +22,12 @@ window.onload = function () {
                         }
                         terit.setAttribute('buildings', 'outpost');
                         bank -= 6;
-                        goldSpan.innerText = `${bank}`;
-                        buildOutpostBtn.setAttribute('disabled', true);
+                        goldSpan.textContent = `${bank}`;
+                        buildOutpostBtn.setAttribute('disabled', 'true');
                     });
                 }
                 if (bank - 12 >= 0) {
-                    const buildBarracksBtn = node.querySelector('#build-barracks-btn');
+                    const buildBarracksBtn = node.querySelector('#build-barracks-btn')!;
                     buildBarracksBtn.removeAttribute('disabled');
                     buildBarracksBtn.addEventListener('click', () => {
                         if (bank - 12 < 0) {
@@ -35,18 +35,18 @@ window.onload = function () {
                         }
                         terit.setAttribute('buildings', 'barracks');
                         bank -= 6;
-                        goldSpan.innerText = `${bank}`;
-                        buildBarracksBtn.setAttribute('disabled', true);
+                        goldSpan.textContent = `${bank}`;
+                        buildBarracksBtn.setAttribute('disabled', 'true');
                     });
                 }
             } else if (terit.buildings.contains('barracks') && bank > 0) {
-                const deployBtn = node.querySelector('#deploy-btn');
-                const deployInput = node.querySelector('#deploy-input');
+                const deployBtn = node.querySelector('#deploy-btn')!;
+                const deployInput = node.querySelector('#deploy-input')! as HTMLInputElement;
                 deployInput.removeAttribute('disabled');
                 deployInput.addEventListener('change', () => {
                     const amount = deployInput.value;
                     const cost = amount;
-                    deployBtn.innerText = `Deploy ${amount} troops (-${cost})`;
+                    deployBtn.textContent = `Deploy ${amount} troops (-${cost})`;
                 });
                 deployBtn.removeAttribute('disabled');
                 deployBtn.addEventListener('click', () => {
@@ -59,35 +59,36 @@ window.onload = function () {
                         throw new Error('insufficient funds');
                     }
 
-                    let troops = terit.querySelector('map-troops');
+                    let troops = terit.querySelector('map-troops') as MapTroops;
                     if (troops === null) {
-                        troops = terit.appendChild(document.createElement('map-troops'));
+                        troops = terit.appendChild(document.createElement('map-troops')) as MapTroops;
                     }
             
                     bank -= cost;
-                    goldSpan.innerText = `${bank}`;
-                    troops.setAttribute('amount', troops.amount + amount);
+                    goldSpan.textContent = `${bank}`;
+                    troops.setAttribute('amount', `${troops.amount + amount}`);
                     
-                    deployInput.value = 1;
+                    deployInput.value = '1';
                     deployInput.dispatchEvent(new Event('change'));
                 });
             }
-            document.getElementById('contextual-panel').appendChild(node);
+            document.getElementById('contextual-panel')!.appendChild(node);
         } else if (event.target instanceof MapTroops) {
             const troops = event.target;
-            const tmpl = document.getElementById('troops-control-panel-tmpl');
-            const node = tmpl.content.cloneNode(true);
-            node.querySelector('#territory-name').innerText = troops.parentElement.name;
-            node.querySelector('#troop-amount').innerText = troops.amount;
-            node.querySelector('#troop-moves').innerText = troops.moves;
-            document.getElementById('contextual-panel').appendChild(node);
+            const tmpl = document.getElementById('troops-control-panel-tmpl')! as HTMLTemplateElement;
+            const node = tmpl.content.cloneNode(true) as HTMLElement;
+            const terit = troops.parentElement! as MapTerritory;
+            node.querySelector('#territory-name')!.textContent = terit.name;
+            node.querySelector('#troop-amount')!.textContent = `${troops.amount}`;
+            node.querySelector('#troop-moves')!.textContent = `${troops.moves}`;
+            document.getElementById('contextual-panel')!.appendChild(node);
         }
     });
 
     mapView.addEventListener('map:deselected', () => {
-        const panel = document.getElementById('contextual-panel');
+        const panel = document.getElementById('contextual-panel')!;
         while (panel.firstChild) {
-            panel.removeChild(panel.lastChild);
+            panel.removeChild(panel.lastChild!);
         }
     });
 };
